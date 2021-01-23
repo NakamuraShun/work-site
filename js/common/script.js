be13e08a3dd763048c2ee64fa19a2ae9aa089072
中村隼 <nakamurashun@SHUN.local>
2021-01-23 18:50:35 +0900

    “msg”

--- js/common/script.js
+++ js/common/script.js
@@ -213,6 +213,31 @@ $(function ()
     });
 
 
+/* :hover invalid, when use touchdevise
+-------------------------------------------------------------------- */
+    var touch = 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+    if (touch)
+    {
+        try
+        {
+            for (var si in document.styleSheets)
+            {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--)
+                {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+
+                    if (styleSheet.rules[ri].selectorText.match(':hover'))
+                    {
+                        styleSheet.deleteRule(ri);
+                    }
+                }
+            }
+        } catch (ex) { }
+    }
 
 
 });
